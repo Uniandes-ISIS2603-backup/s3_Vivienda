431947d4dd7fd2581a529f9190c23c1a518a7ac9
estudiante <estudiante@ISIS2603-315.sis.virtual.uniandes.edu.co>
2018-08-27 01:38:47 -0500

    Clase Contrato agregada. Toca cambiar su dependencia ya que no es recurso raiz.

---
+++ s3_vivienda-back/src/main/java/co/edu/uniandes/csw/vivienda/entities/ContratoEntity.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.vivienda.entities;
+
+import java.io.Serializable;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author estudiante
+ */
+public class ContratoEntity extends BaseEntity implements Serializable {
+
+    private static final Logger LOGGER = Logger.getLogger(ContratoEntity.class.getName());
+    private Long id;
+    private String fechaInicio;
+    private String fechaFin;
+    private String metodoPago;
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the fechaInicio
+     */
+    public String getFechaInicio() {
+        return fechaInicio;
+    }
+
+    /**
+     * @param fechaInicio the fechaInicio to set
+     */
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    /**
+     * @return the fechaFin
+     */
+    public String getFechaFin() {
+        return fechaFin;
+    }
+
+    /**
+     * @param fechaFin the fechaFin to set
+     */
+    public void setFechaFin(String fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    /**
+     * @return the metodoPago
+     */
+    public String getMetodoPago() {
+        return metodoPago;
+    }
+
+    /**
+     * @param metodoPago the metodoPago to set
+     */
+    public void setMetodoPago(String metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+    
+}
