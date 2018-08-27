203938d24700784258e9792abda275ce7ad96b7f
dgiraldom95 <d.giraldo12@uniandes.edu.co>
2018-08-26 23:24:15 -0500

    ViviendaDTO, ViviendaResource, ViviendaEntity

---
+++ s3_vivienda-back/src/main/java/co/edu/uniandes/csw/vivienda/persistence/ViviendaPersistence.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.vivienda.persistence;
+
+/**
+ * Clase que maneja la persistencia de la entidad vivienda
+ * @author DANIEL
+ */
+public class ViviendaPersistence {
+    
+}
