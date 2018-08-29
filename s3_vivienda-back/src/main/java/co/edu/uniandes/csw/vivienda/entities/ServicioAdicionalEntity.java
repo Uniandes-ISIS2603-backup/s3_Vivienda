5815165f384fd992355006e32815ed30c4e4975e
estudiante <estudiante@ISIS2603-376.sis.virtual.uniandes.edu.co>
2018-08-29 14:23:43 -0500

    Universidad y ServicioAdicional Back

---
+++ s3_vivienda-back/src/main/java/co/edu/uniandes/csw/vivienda/entities/ServicioAdicionalEntity.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.vivienda.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author estudiante
+ */
+@Entity
+@XmlRootElement
+public class ServicioAdicionalEntity extends BaseEntity implements Serializable{
+
+  private String nombre;
+  private String descripcion;
+  private Float costo; 
+  
+  public String getNombre() {
+        return nombre;
+  }
+  
+  public String getDescripcion() {
+        return descripcion;
+  }
+  
+  public Float getCosto() {
+        return costo;
+  }
+
+  public void setNombre(String nombre) {
+        this.nombre = nombre;
+  }
+  
+  public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+  }
+
+ public void setCosto(Float costo) {
+        this.costo = costo;
+  }
+     
+}
---
+++ s3_vivienda-back/src/main/java/co/edu/uniandes/csw/vivienda/entities/UniversidadEntity.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.vivienda.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author estudiante
+ */
+@Entity
+@XmlRootElement
+public class UniversidadEntity extends BaseEntity implements Serializable
+{
+    private String nombre;
+    private Float latitud;
+    private Float longitud;
+    
+     public String getNombre() {
+        return nombre;
+    }
+     
+      public float getLatitud() {
+        return latitud;
+    }
+
+    public float getLongitud() {
+        return longitud;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+     public void setLatitud(Float latitud) {
+        this.latitud = latitud;
+    }
+
+    public void setLongitud(Float longitud) {
+        this.longitud = longitud;
+    }
+    
+}
