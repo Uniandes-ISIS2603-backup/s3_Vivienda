66130360cc73a481316bca15de067b3a00575f18
Daniel <estudiante@ISIS2603-299.sis.virtual.uniandes.edu.co>
2018-09-03 10:45:06 -0500

    Test ViviendaPersistence

--- s3_vivienda-back/src/test/java/persistence/ViviendaPersistenceTest.java
+++ s3_vivienda-back/src/test/java/persistence/ViviendaPersistenceTest.java
@@ -5,10 +5,77 @@
  */
 package persistence;
 
+import co.edu.uniandes.csw.vivienda.entities.ViviendaEntity;
+import co.edu.uniandes.csw.vivienda.persistence.ViviendaPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
 /**
  *
  * @author estudiante
  */
+@RunWith(Arquillian.class)
 public class ViviendaPersistenceTest {
+    @Inject
+    private ViviendaPersistence persistence;
     
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Inject
+    UserTransaction utx;
+    
+    private List<ViviendaEntity> data = new ArrayList<ViviendaEntity>();
+    
+    @Deployment
+    public static JavaArchive createDeployement(){
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(ViviendaEntity.class.getPackage())
+                .addPackage(ViviendaPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+    private void clearData() {
+        em.createQuery("delete from ViviendaEntity").executeUpdate();
+    }
+    
+    private void insertData(){
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 5; i++) {
+            ViviendaEntity ent = factory.manufacturePojo(ViviendaEntity.class);
+            em.persist(ent);
+            data.add(ent);            
+        }
+    }
 }
