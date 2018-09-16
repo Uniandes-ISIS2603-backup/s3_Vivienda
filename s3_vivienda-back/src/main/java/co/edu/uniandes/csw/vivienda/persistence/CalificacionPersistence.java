dd58d370d1dc2194e0c78ed62300cafcb942309a
Juan Manuel Castillo <jm.castillo12>
2018-09-16 14:28:17 -0500

    Documentación y unión de resources y lógica de calificacion y estudiante

--- s3_vivienda-back/src/main/java/co/edu/uniandes/csw/vivienda/persistence/CalificacionPersistence.java
+++ s3_vivienda-back/src/main/java/co/edu/uniandes/csw/vivienda/persistence/CalificacionPersistence.java
@@ -26,34 +26,74 @@ public class CalificacionPersistence {
     @PersistenceContext(unitName = "UniviviendaPU")
     protected EntityManager em;
     
+    /**
+     * Método para persisitir la entidad en la base de datos.
+     *
+     * @param entity objeto calificacion que se creará en la base de datos
+     * @return devuelve la entidad creada con un id dado por la base de datos.
+     */
     public CalificacionEntity create(CalificacionEntity entity){
         em.persist(entity);
         return entity;
     }
+    
+    /**
+     * Devuelve todas las calificaciones de la base de datos.
+     *
+     * @return una lista con todas las calificaciones que encuentren en la base de
+     * datos, "select u from CalificacionEntity u" es como un "select * from
+     * CalificacionEntity;" - "SELECT * FROM table_name" en SQL.
+     */
     public List<CalificacionEntity> findAll(){
         TypedQuery query = em.createQuery("select u from CalificacionEntity u", CalificacionEntity.class);
         return query.getResultList();
     }
+    
+    /**
+     * Devuelve todas las calificaciones de una vivienda de la base de datos.
+     *
+     * @param viviendaId
+     * @return una lista con todas las calificaciones de una vivienda que encuentren en la base de
+     * datos, "select u from CalificacionEntity u" es como un "select * from
+     * CalificacionEntity;" - "SELECT * FROM table_name" en SQL.
+     */
     public List<CalificacionEntity> findAllByVivienda(Long viviendaId){
         TypedQuery query = em.createQuery("select u from CalificacionEntity u where (u.vivienda.id = :viviendaId)", CalificacionEntity.class);
         query.setParameter("viviendaId", viviendaId);
         return query.getResultList();
     }
+    
+    /**
+     * Devuelve todas las calificaciones de un estudiante de la base de datos.
+     *
+     * @param estudianteId
+     * @return una lista con todas las calificaciones de un estudiante que encuentren en la base de
+     * datos, "select u from CalificacionEntity u" es como un "select * from
+     * CalificacionEntity;" - "SELECT * FROM table_name" en SQL.
+     */
     public List<CalificacionEntity> findAllByEstudiante(Long estudianteId){
         TypedQuery query = em.createQuery("select u from CalificacionEntity u where (u.estudiante.id = :estudianteId)", CalificacionEntity.class);
         query.setParameter("estudianteId", estudianteId);
         return query.getResultList();
     }
+    
+    /**
+     * Busca si hay alguna calificacion con el id que se envía de argumento
+     *
+     * @param calificacionId: id correspondiente a la calificacion buscada.
+     * @return una calificacion.
+     */
     public CalificacionEntity find(Long calificacionId){
         return em.find(CalificacionEntity.class, calificacionId);
     }
-    public CalificacionEntity update(CalificacionEntity entity){
-        return em.merge(entity);
-    }
-    public void delete(Long calificacionId){
-        CalificacionEntity entity = em.find(CalificacionEntity.class, calificacionId);
-        em.remove(entity);
-    }
+    
+    /**
+     * Busca si hay alguna calificacion, de una vivienda, con el id que se envía de argumento
+     *
+     * @param viviendaId: id de la vivienda
+     * @param calificacionId: id correspondiente a la calificacion buscada.
+     * @return una calificacion.
+     */
     public CalificacionEntity findByVivienda(Long viviendaId, Long calificacionId){
         TypedQuery query = em.createQuery("select u from CalificacionEntity u where (u.vivienda.id = :viviendaId) and (u.id = :calificacionId)", CalificacionEntity.class);
         query.setParameter("viviendaId", viviendaId);
@@ -67,6 +107,14 @@ public class CalificacionPersistence {
             result = resultList.get(0);
         return result;
     }
+    
+    /**
+     * Busca si hay alguna calificacion, de un estudiante, con el id que se envía de argumento
+     *
+     * @param estudianteId: id del estudinate
+     * @param calificacionId: id correspondiente a la calificacion buscada.
+     * @return una calificacion.
+     */
     public CalificacionEntity findByEstudiante(Long estudianteId, Long calificacionId){
         TypedQuery query = em.createQuery("select u from CalificacionEntity u where (u.estudiante.id = :estudianteId) and (u.id = :calificacionId", CalificacionEntity.class);
         query.setParameter("estudianteId", estudianteId);
@@ -79,5 +127,29 @@ public class CalificacionPersistence {
         else
             result = resultList.get(0);
         return result;
+    }
+    
+    /**
+     * Actualiza una calificacion.
+     *
+     * @param entity: la calificacion que viene con los nuevos cambios.
+     * Por ejemplo la descripción pudo cambiar. En ese caso, se haria uso del método
+     * update.
+     * @return una calificacion con los cambios aplicados.
+     */
+     public CalificacionEntity update(CalificacionEntity entity){
+        return em.merge(entity);
+    }
+     
+     /**
+     *
+     * Borra una calificacion de la base de datos recibiendo como argumento el id
+     * de la calificacion
+     *
+     * @param calificacionId: id correspondiente a la calificacion a borrar.
+     */
+    public void delete(Long calificacionId){
+        CalificacionEntity entity = em.find(CalificacionEntity.class, calificacionId);
+        em.remove(entity);
     }
 }
--- s3_vivienda-back/src/main/java/co/edu/uniandes/csw/vivienda/persistence/EstudiantePersistence.java
+++ s3_vivienda-back/src/main/java/co/edu/uniandes/csw/vivienda/persistence/EstudiantePersistence.java
@@ -6,18 +6,16 @@
 package co.edu.uniandes.csw.vivienda.persistence;
 
 import co.edu.uniandes.csw.vivienda.entities.EstudianteEntity;
-import java.util.Collection;
 import java.util.List;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 /**
  *
- * @author Juan Manuel Castillo
+ * @estudiante Juan Manuel Castillo
  */
 @Stateless
 public class EstudiantePersistence {
@@ -26,25 +24,46 @@ public class EstudiantePersistence {
     @PersistenceContext(unitName = "UniviviendaPU")
     protected EntityManager em;
     
+    /**
+     * Crea un autor en la base de datos
+     *
+     * @param estudianteEntity objeto estudiante que se creará en la base de datos
+     * @return devuelve la entidad creada con un id dado por la base de datos.
+     */
     public EstudianteEntity create(EstudianteEntity estudianteEntity){
         em.persist(estudianteEntity);
         return estudianteEntity;
     }
+    
+    /**
+     * Devuelve todos los estudiantes de la base de datos.
+     *
+     * @return una lista con todos los estudiantes que encuentre en la base de
+     * datos, "select u from EstudianteEntity u" es como un "select * from
+     * EstudianteEntity;" - "SELECT * FROM table_name" en SQL.
+     */
     public List<EstudianteEntity> findAll(){
         TypedQuery query = em.createQuery("select u from EstudianteEntity u", EstudianteEntity.class);
         return query.getResultList();
     }
+    
+    /**
+     * Busca si hay algún estudiante con el id que se envía de argumento
+     *
+     * @param estudianteId: id correspondiente a la estudiante buscada.
+     * @return un estudiante.
+     */
     public EstudianteEntity find(Long estudianteId){
         return em.find(EstudianteEntity.class, estudianteId);
     }
-    public EstudianteEntity update(EstudianteEntity estudianteEntity){
-        return em.merge(estudianteEntity);
-    }
-    public void delete(Long estudianteId){
-        EstudianteEntity estudianteEntity = em.find(EstudianteEntity.class, estudianteId);
-        em.remove(estudianteEntity);
-    }
-    public Collection<EstudianteEntity> findByNombre(String nombre){
+    
+    /**
+     * Busca si hay algún estudiante con el id que se envía de argumento
+     *
+     * @param nombre: nombre correspondiente al estudiante buscado.
+     * @return lista de estudiantes con el nombre dado por parámetro.
+     */
+    public List<EstudianteEntity> findByNombre(String nombre){
         TypedQuery query = em.createQuery("select u from EstudianteEntity u where u.nombre = :nombre", EstudianteEntity.class);
         query.setParameter("nombre", nombre);
         
@@ -53,6 +72,13 @@ public class EstudiantePersistence {
             return null;
         return resultList;
     }
+    
+    /**
+     * Busca si hay algún estudiante con el login que se envía de argumento
+     *
+     * @param login: login correspondiente al estudiante buscado.
+     * @return un estudiante.
+     */
     public EstudianteEntity findByLogin(String login){
         TypedQuery query = em.createQuery("select u from EstudianteEntity u where u.login = :login", EstudianteEntity.class);
         query.setParameter("login", login);
@@ -66,4 +92,26 @@ public class EstudiantePersistence {
         return result;
     }
     
+    /**
+     * Actualiza un estudiante.
+     *
+     * @param estudianteEntity: la estudiante que viene con los nuevos cambios. Por
+     * ejemplo el nombre pudo cambiar. En ese caso, se haria uso del método
+     * update.
+     * @return un estudiante con los cambios aplicados.
+     */
+    public EstudianteEntity update(EstudianteEntity estudianteEntity){
+        return em.merge(estudianteEntity);
+    }
+    
+    /**
+     * Borra un estudiante de la base de datos recibiendo como argumento el id de
+     * la estudiante
+     *
+     * @param estudianteId: id correspondiente a la estudiante a borrar.
+     */
+    public void delete(Long estudianteId){
+        EstudianteEntity estudianteEntity = em.find(EstudianteEntity.class, estudianteId);
+        em.remove(estudianteEntity);
+    }
 }
