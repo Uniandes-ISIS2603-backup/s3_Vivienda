b4b3c7ef35e18b91ce2597d9048b55ad14965529
estudiante <pa.molina@uniandes.edu.co>
2018-09-16 21:39:27 -0500

    ServicioAdicional Correggido

--- s3_vivienda-api/src/main/java/co/edu/uniandes/csw/vivienda/dtos/ServicioAdicionalDTO.java
+++ s3_vivienda-api/src/main/java/co/edu/uniandes/csw/vivienda/dtos/ServicioAdicionalDTO.java
@@ -13,9 +13,16 @@ import co.edu.uniandes.csw.vivienda.entities.ServicioAdicionalEntity;
  */
 public class ServicioAdicionalDTO {
     
+    private Long id;
     private String nombre;
     private String descripcion;
     private Float costo;
+    
+    /*
+    * Relación a una vivienda
+    * dado que esta tiene cardinalidad 1.
+    */
+    private ViviendaDTO vivienda;
     
     public ServicioAdicionalDTO(){
         
@@ -24,20 +31,48 @@ public class ServicioAdicionalDTO {
     public ServicioAdicionalDTO( ServicioAdicionalEntity servicioAdicionalEntity){
         
         if(servicioAdicionalEntity!=null){
+            this.id = servicioAdicionalEntity.getId();
             this.nombre = servicioAdicionalEntity.getNombre();
             this.descripcion = servicioAdicionalEntity.getDescripcion();
             this.costo = servicioAdicionalEntity.getCosto();
+            if (servicioAdicionalEntity.getVivienda() != null) {
+                this.vivienda = new ViviendaDTO(servicioAdicionalEntity.getVivienda());
+            } else {
+                this.vivienda = null;
+            }
             
         }
     } 
         public ServicioAdicionalEntity toEntity(){
         ServicioAdicionalEntity servicioAdicionalEntity = new ServicioAdicionalEntity();
         
-        servicioAdicionalEntity.setNombre(nombre);
-        servicioAdicionalEntity.setDescripcion(descripcion);
-        servicioAdicionalEntity.setDescripcion(descripcion);
+        servicioAdicionalEntity.setId(this.id);
+        servicioAdicionalEntity.setNombre(this.nombre);
+        servicioAdicionalEntity.setDescripcion(this.descripcion);
+        servicioAdicionalEntity.setDescripcion(this.descripcion);
+         if (this.vivienda != null) {
+            servicioAdicionalEntity.setVivienda(this.vivienda.toEntity());
+        }
 
         return servicioAdicionalEntity;
+    }
+        
+    /**
+     * Devuelve el ID del servicio.
+     *
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Modifica el ID de la reseña.
+     *
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
     }
     
     public String getNombre() {
@@ -62,8 +97,23 @@ public class ServicioAdicionalDTO {
 
     public void setCosto(Float costo) {
         this.costo = costo;
+    }      
+
+    /**
+     * Devuelve la vivienda asociada a este servicio.
+     *
+     * @return the vivienda
+     */
+    public ViviendaDTO getVivienda() {
+        return vivienda;
     }
-    
-    
-           
+
+    /**
+     * Modifica la vivienda asociada a este servicio.
+     *
+     * @param vivienda the book to set
+     */
+    public void setVivienda(ViviendaDTO vivienda) {
+        this.vivienda = vivienda;
+    }    
 }
