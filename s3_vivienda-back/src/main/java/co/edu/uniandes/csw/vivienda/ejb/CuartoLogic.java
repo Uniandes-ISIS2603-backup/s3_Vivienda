fff20c0ea2464f13c71a4b895bffe8067f8dc884
dgiraldom95 <d.giraldo12@uniandes.edu.co>
2018-11-28 10:56:39 -0500

    Generar datos cuarto ocupado

--- s3_vivienda-back/src/main/java/co/edu/uniandes/csw/vivienda/ejb/CuartoLogic.java
+++ s3_vivienda-back/src/main/java/co/edu/uniandes/csw/vivienda/ejb/CuartoLogic.java
@@ -100,8 +100,9 @@ public class CuartoLogic {
         for (int i = 0; i < numeroCuartos; i++) {
             CuartoEntity cuarto = new CuartoEntity();
             int costoArriendo = (800 + new Random().nextInt(1000)) * 1000;
-            cuarto.setOcupado(false);
+            int ocupado = new Random().nextInt(100) % 2;
 
+            cuarto.setOcupado(ocupado == 0);
             cuarto.setNombre("Cuarto " + i);
             cuarto.setCostoArriendo(costoArriendo);
             cuarto.setDescripcion("Un cuarto común");
